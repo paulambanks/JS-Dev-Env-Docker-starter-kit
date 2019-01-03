274745067fe4756b6e4c9151fe2978bb6e2d5b64
Paula Banks <bpaula92@gmail.com>
2019-01-03 20:48:30 +0000

    ESLint Set Up

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,29 +1,29 @@
 // Using ES6 features
 import path from 'path';
 
-module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    entry: [
-        path.resolve(__dirname, 'src/index')
-    ],
-    output: {
-        path: path.resolve(__dirname, 'src'),
-        publicPath: '/',
-        filename: 'bundle.js'
+export default {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  target: 'web',
+  plugins: [],
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
     },
-    target: 'web',
-    plugins: [],
-    module: {
-        rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            }
-        ]
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
     }
+    ]
+  }
 }
