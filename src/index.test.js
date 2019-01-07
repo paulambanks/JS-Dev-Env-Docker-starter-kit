8ef2e54435db42b64fdd16f8a198d23401b94d25
Paula Banks <bpaula92@gmail.com>
2019-01-07 20:33:36 +0000

    Initiated testing setup with Mocha, Chai and JSDOM with basic index.test.js

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+// An example of a passing test. Write your tests here.
+import { expect } from 'chai';
+import fs from 'fs';
+import jsdom from 'jsdom';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
