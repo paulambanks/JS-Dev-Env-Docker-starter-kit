d150c4fe91afbfc9dc2bfeb1098ef951aa8497eb
Paula Banks <bpaula92@gmail.com>
2019-01-08 20:37:39 +0000

    Updated index.test.js with JSDOM test

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,21 @@
 // An example of a passing test. Write your tests here.
 import { expect } from 'chai';
-import fs from 'fs';
-import jsdom from 'jsdom';
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 describe('Our first test', () => {
   it('should pass', () => {
     expect(true).to.equal(true);
   });
 });
+
+describe('index.html', () => { // eslint-disable-line
+  it('should say hello', (done) => { // eslint-disable-line
+    const options = {}
+    JSDOM.fromFile('./src/index.html', options).then(dom => {
+      const h1 = dom.window.document.getElementsByTagName('h1')[0]
+      expect(h1.innerHTML).to.equal('Hello World!')
+      done()
+    }).catch(done)
+  })
+})
